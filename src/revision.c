9f871999c480ae80d1ec40a1c0a3fb1d88218ac8
sunqueen <sunqueen@paran.com>
2017-07-02 07:50:53 +0900

    vlc-2.2.6 compile with VS2015
    list of use
    
    vlc-contrib-i686-w64-mingw32-20160218.tar.bz2
    qt4 : 4.8.5
    game-music-emu-0.6.0.tar.bz2
    harfbuzz-0.9.19.tar.bz2
    libmodplug-0.8.8.5.tar.gz
    live.2011.12.23.tar.gz
    sidplay-libs-2.1.1.tar.gz

---
+++ src/revision.c
@@ -0,0 +1,1 @@
+const char psz_vlc_changeset[] = "2.2.6-0-g1aae78981c";
---
+++ win32/include/liveMedia_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "liveMedia" library
+// Copyright (c) 1996-2016 Live Networks, Inc.  All rights reserved.
+
+#ifndef _LIVEMEDIA_VERSION_HH
+#define _LIVEMEDIA_VERSION_HH
+
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2016.01.12"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1452556800
+
+#endif
